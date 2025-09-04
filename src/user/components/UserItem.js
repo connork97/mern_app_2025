fb646749848c77248b8629d5e01a3a1d6dd93700
Connor Kormos <connorkormos97@gmail.com>
2025-09-04 14:20:48 -0700

    added side drawer functionality and styling for mobile view of navlinks

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Avatar from "../../shared/components/UIElements/Avatar";
-import Card from "../../shared/components/Card";
+import Card from "../../shared/components/UIElements/Card";
 
 import "./UserItem.css";
 
